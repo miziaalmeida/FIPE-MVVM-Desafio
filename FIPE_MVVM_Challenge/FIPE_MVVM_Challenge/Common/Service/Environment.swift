97ae2399dc35c72877b46efbe188169e5b0cd2ee
Mízia Lima <mizia.alima@gmail.com>
2024-02-05 16:16:53 -0300

    feat: add instructions for challenge to Jeiel

--- FIPE_MVVM_Challenge/FIPE_MVVM_Challenge/Common/Service/Environment.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-enum Environment {
-    static let baseUrl = "https://parallelum.com.br/fipe/api/v1"
-}
--- FIPE_MVVM_Challenge/Pods/Target Support Files/Pods-FIPE_MVVM_Challenge/Pods-FIPE_MVVM_Challenge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FIPE_MVVM_Challenge : NSObject
-@end
-@implementation PodsDummy_Pods_FIPE_MVVM_Challenge
-@end
